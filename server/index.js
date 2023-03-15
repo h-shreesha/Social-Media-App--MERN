cb2007dcd544e2d7f0fe9712129736c17b33593d
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-15 22:04:59 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
-import  Express  from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose"
-import cors from "cors"
-import dotenv from "dotenv"
-import multer from "multer"
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
+import Express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// ?CONFIGURATIONS
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.__dirname(__filename)
+dotenv.config();
+
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy}))
