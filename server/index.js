4ea9bfc672538881ddd962c9fcd3e8aff3bf8914
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-16 21:56:42 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -12,10 +12,26 @@ import { fileURLToPath } from 'url';
 // ?CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.__dirname(__filename)
+const __dirname = path.__dirname(__filename);
 dotenv.config();
 
 const app = express();
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy}))
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+// ? FILE STORAGE
+
+const storage = multer.diskStorage({
+    destination: function (req, res, cb) {
+        cb(null, 'public/assets');
+    },
+    filename: function (req, res, cb) {
+        cb(null, file.originalname);
+    },
+});
