309ff21d0659f601e3dc97c30a5af98f8b7a33c9
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-24 22:16:31 +0530

    post router

--- server/App.js
+++ server/App.js
@@ -9,8 +9,11 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
-import userRoutes from './routes/users.js'
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import { createPost } from './controllers/posts.js';
 import { register } from './controllers/auth.js';
+import { verifyToken } from './middleware/auth.js';
 
 // ?CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -40,10 +43,12 @@ const upload = multer({ storage });
 
 // ?ROUTES WITH FILE
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 // ? ROUTES
 app.use('/auth', authRoutes);
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 //? MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
