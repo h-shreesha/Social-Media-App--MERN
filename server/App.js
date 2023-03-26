58d2dc34659b34efbb3789128682e51260b783ef
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-26 11:42:54 +0530

    updated

--- server/App.js
+++ server/App.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/post.js';
 import { createPost } from './controllers/posts.js';
 import { register } from './controllers/auth.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Posts.js';
+import { users, posts } from './data/index.js';
 
 // ?CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -61,6 +64,10 @@ mongoose
         app.listen(PORT, () =>
             console.log(`Server is listening on port : ${PORT}`)
         );
+
+        // ? ADD DATA ONE TIME
+        // User.insertMany(users);
+        // Post.insertMany(posts);
     })
     .catch((error) => console.log(`${error} did not connect`));
 
