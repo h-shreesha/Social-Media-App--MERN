f76953ef43ca67adad59fc47f825f7d98c1bc3c8
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-27 21:57:15 +0530

    update

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from './state';
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import {
     persistStore,
@@ -13,19 +13,37 @@ import {
     PAUSE,
     PURGE,
     REGISTER,
+    PERSIST,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
-const persistConfig = {key: 'root', storage, version: 1}
-const persistedReducer = persistReducer(persistConfig, authReducer)
+const persistConfig = { key: 'root', storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
-  reducer: persis
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => {
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        });
+    },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+          <PersistGate loading= {null} persistor={persistStore(store)}></PersistGate>
+          <App />
+        </Provider>
     </React.StrictMode>
 );
